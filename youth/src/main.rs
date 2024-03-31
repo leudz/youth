ca7dcbdb22b8fd8476db43537400cd3dcf4693ea
leudz <dylan.ancel@gmail.com>
2024-03-31 13:32:20 +0200

    Disable instruct

--- youth/src/main.rs
+++ youth/src/main.rs
@@ -16,15 +16,16 @@ fn main() {
     // rag.save();
 
     let mut llm = LLM::init(Model::Mistral, "Leudz", "Emma");
+    // llm.disable_tts();
 
-    llm.history_mut().set_instruction(
-        "- I'm going to describe you modules.\n\
-         - You will ask me questions to collect details on the module.\n\
-         - You can ask multiple questions. Ask a single question at a time.\n\
-         - Based on the instructions in the context and the description I gave you, tell me how to solve the module.\n\
-         - Rely only on the information I give you.
-         - Keep your replies concise, we are under a very strict time limit.
-         - If I tell you that you made a mistake, don't get stuck on your first answer and simply go through the instructions again to find the right answer.");
+    // llm.history_mut().set_instruction(
+    //     "- I'm going to describe you modules.\n\
+    //      - You will ask me questions to collect details on the module.\n\
+    //      - You can ask multiple questions. Ask a single question at a time.\n\
+    //      - Based on the instructions in the context and the description I gave you, tell me how to solve the module.\n\
+    //      - Rely only on the information I give you.
+    //      - Keep your replies concise, we are under a very strict time limit.
+    //      - If I tell you that you made a mistake, don't get stuck on your first answer and simply go through the instructions again to find the right answer.");
 
     println!("Ready");
 
@@ -48,7 +49,9 @@ fn main() {
             continue;
         }
 
-        llm.history_mut().set_context(rag.update_context(&input));
+        if !llm.history_mut().instruction().is_empty() {
+            llm.history_mut().set_context(rag.update_context(&input));
+        }
 
         llm.history_mut().add(input.clone(), Speaker::User);
         input.clear();
